34bf3fe34c4d4a95ad73a9118ec890814e43a707
zyj <zhongyinjiebit@sina.com>
2020-12-30 09:00:18 +0800

    function

--- ch-06 Branching Statements and Logical Operators/6.10 Chapter Review/input_buffer.cpp
+++ ch-06 Branching Statements and Logical Operators/6.10 Chapter Review/input_buffer.cpp
@@ -11,18 +11,19 @@ int main(int argc, char *argv[])
 	{
 		cout << ch;
 		ct1++;
-		if (ch = '$')
-		  ct2++;
-		cout << ch;
+		//if (ch == '$')
+		//  ct2++;
+		//cout <<"("<< ch << ")";
 	}
 	cout << endl <<"ct1 = " << ct1 << ", ct2 = " << ct2 << "\n";
 	return 0;
 }
 
+//input：
 //Hi!
 //Send $10 or $20 now!
 
-//output
+//output:
 //H$i$!$
 //$S$e$n$d$ $
 //ct1 = 9, ct2 = 9
