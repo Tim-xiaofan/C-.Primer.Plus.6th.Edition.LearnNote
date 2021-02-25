132408a2dfb62a1f03c3b7515fbd7643609b9d69
zyj <1305696765@qq.com>
2021-02-25 08:01:06 -0500

    exception specifications

--- ch-15 Friends, Exceptions, and More/15.03 Exceptions/exception inheritance/main.cpp
+++ ch-15 Friends, Exceptions, and More/15.03 Exceptions/exception inheritance/main.cpp
@@ -6,20 +6,73 @@
 int main()
 {
 	using std::cout;
+	using std::cin;
 	using std::endl;
-	Sales sales(9.15);
-	LabeledSales lsales("233", 9);
+
+	double vals1[12] =
+	{
+		1220, 1100, 1122, 2212, 1232, 2334,
+		2884, 2393, 3302, 2922, 3002, 3544
+	};
+	double vals2[12] =
+	{
+		12, 11, 22, 21, 32, 34,
+		28, 29, 33, 29, 32, 35
+	};
+	Sales sales1(2011, vals1, 12);
+	LabeledSales sales2("Blogstar",2012, vals2, 12 );
+
+	cout << "First try block:\n";
 	try
 	{
-		//cout << sales[19] << endl;
-		cout << lsales[19] << endl;
+		int i;
+		cout << "Year = " << sales1.Year() << endl;
+		for (i = 0; i < 12; ++i)
+		{
+			cout << sales1[i] << ' ';
+			if (i % 6 == 5)
+			  cout << endl;
+		}
+		cout << "Year = " << sales2.Year() << endl;
+		cout << "Label = " << sales2.Label() << endl;
+		for (i = 0; i <= 12; ++i)
+		{
+			cout << sales2[i] << ' ';
+			if (i % 6 == 5)
+			  cout << endl;
+		}
+		cout << "End of try block 1.\n";
 	}
-	catch(LabeledSales::nbad_index & nbi)
+	catch(LabeledSales::nbad_index & bad)
 	{
-		cout << nbi.what() << endl;
+		cout << bad.what();
+		cout << "Company: " << bad.label_val() << endl;
+		cout << "bad index: " << bad.bi_val() << endl;
 	}
-	catch(Sales::bad_index & bi)
+	catch(Sales::bad_index & bad)
 	{
-		cout << bi.what() << endl;
+		cout << bad.what();
+		cout << "bad index: " << bad.bi_val() << endl;
 	}
+
+	cout << "\nNext try block:\n";
+	try
+	{
+		sales2[2] = 37.5;
+		sales1[20] = 23345;
+		cout << "End of try block 2.\n";
+	}
+	catch(LabeledSales::nbad_index & bad)
+	{
+		cout << bad.what();
+		cout << "Company: " << bad.label_val() << endl;
+		cout << "bad index: " << bad.bi_val() << endl;
+	}
+	catch(Sales::bad_index & bad)
+	{
+		cout << bad.what();
+		cout << "bad index: " << bad.bi_val() << endl;
+	}
+	cout << "done\n";
+	return 0;
 }
--- ch-15 Friends, Exceptions, and More/15.03 Exceptions/try_catch.cpp
+++ ch-15 Friends, Exceptions, and More/15.03 Exceptions/try_catch.cpp
@@ -1,8 +1,9 @@
-/** 20210222 21:57, zyj, GuangDong*/
+/* 20210222 21:57, zyj, GuangDong*/
 //error1.cpp -- using the abort() function
 #include <iostream>
 #include <cstdlib>
-double hmean(double a, double b);
+/** exception specifications*/
+double hmean(double a, double b) throw(const char *);
 
 int main()
 {
@@ -28,12 +29,18 @@ int main()
 	return 0;
 }
 
-double hmean(double a, double b)
+#define UNEXPECTED
+
+double hmean(double a, double b) throw(const char *)
 {
 	if (a == -b)
 	{
 		std::cout << "untenable arguments to hmean()\n";
+#ifndef UNEXPECTED
 		throw "bad hmean() arguments: a = -b not allowed";
+#else 
+		throw -1;
+#endif
 	}
 	return 2.0 * a * b / (a + b);
 }
