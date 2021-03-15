f7c5a7c6d95fab202754e793bd8eb51717d229b0
zyj <1305696765@qq.com>
2021-03-15 06:43:04 -0700

    cin:get, getline, putback and peek

---
+++ ch-17 Input, Output, and Files/17.03 Input with cin/peeker.cpp
@@ -0,0 +1,49 @@
+/* 20210315 21:22, zyj, GuangDong*/
+// peeker.cpp -- some istream methods
+#include <iostream>
+
+int main()
+{
+	using std::cout;
+	using std::cin;
+	using std::endl;
+
+	// read and echo input up to a # character
+	char ch;
+	while(cin.get(ch)) // terminates on EOF
+	{
+		if (ch != '#')
+		  cout << ch;
+		else/** ch == '#'*/
+		{
+			cin.putback(ch); // reinsert character
+			break;
+		} 
+	}
+
+	if (!cin.eof())
+	{
+		cin.get(ch);
+		cout << endl << ch << " is next input character.\n";
+	}
+	else
+	{
+		cout << "End of file reached.\n";
+		std::exit(0);
+	}
+
+	while(cin.peek() != '#' && cin) // look ahead
+	{
+		cin.get(ch);
+		cout << ch;
+	}
+	if (!cin.eof())
+	{
+		cin.get(ch);
+		cout << endl << ch << " is next input character.\n";
+	}
+	else
+	  cout << "End of file reached.\n";
+
+	return 0;
+}
