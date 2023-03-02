cbde453b3f2229aa70406e5c786d7fa1b1c1cb77
zyj <zhongyinjiebit@sina.com>
2023-03-02 11:42:06 +0800

    chapter 10 exercises: 8

--- ch-10 Objects and Classes/exercises/08.cc
+++ ch-10 Objects and Classes/exercises/08.cc
@@ -7,7 +7,42 @@ using std::cout;
 using std::endl;
 using std::string;
 
+template<typename T>
+void print(const exercises::list<T>& l)
+{
+	for(typename exercises::list<T>::const_iterator p = l.begin();
+				p != l.end();
+				++p)
+	{
+		cout << *p << " ";
+	}
+	cout << endl;
+}
+
 int main(void)
 {
+	int a[4] = {0, 1, 2, 3};
+	exercises::list<int> l;
+	print(l);
+	cout << "empty: " << std::boolalpha << l.empty()  << endl;
+	cout << "full: " << std::boolalpha << l.full()  << endl;
+	l.add(a, 4);
+	print(l);
+	cout << "empty: " << std::boolalpha << l.empty()  << endl;
+	cout << "full: " << std::boolalpha << l.full()  << endl;
+	l.add(a, 4);
+	print(l);
+	cout << "empty: " << std::boolalpha << l.empty()  << endl;
+	cout << "full: " << std::boolalpha << l.full()  << endl;
+
+	for(auto p = l.begin(); p != l.end(); ++p)
+	{
+		*p += 1;
+	}
+	print(l);
+
+	const auto p = l.begin();
+	cout << *p << endl;
+
 	return 0;
 }
--- ch-10 Objects and Classes/exercises/list.h
+++ ch-10 Objects and Classes/exercises/list.h
@@ -2,6 +2,8 @@
 #define _LIST_H_
 
 #include <memory>
+#include <iostream>
+#include <type_traits>
 
 namespace exercises
 {
@@ -15,33 +17,63 @@ struct node
 		:data(d),
 		next(p)
 	{}
+	friend std::ostream& operator<<(std::ostream& os, const node& nd)
+	{
+		return os << "<" << nd.data << ", " << nd.next.get() << ">";
+	}
 };
 
 template<typename T>
 using node_ptr = std::shared_ptr<node<T>>;
 
 template<typename T>
-class iterator
+using const_node_ptr = std::shared_ptr<node<const T>>;
+
+template<typename T, bool Const = false>
+class list_iterator
 {
 	public:
-		iterator(const node_ptr<T>& ptr)
+		using reference_type = typename std::conditional<Const, const T&, T&>::type;
+
+		list_iterator(const node_ptr<T>& ptr)
 			:ptr_(ptr)
 		{}
-		iterator& operator++() { ptr_ = ptr_->next; return *this;};
-		iterator operator++(int) { iterator ret = *this; ptr_ = ptr_->next; return ret; }
-		T& operator*() { return *ptr_;}
+		
+		// ++i
+		list_iterator& operator++()
+		{ ptr_ = ptr_->next; return *this;};
+		
+		// i++
+		list_iterator operator++(int)
+		{ list_iterator ret = *this; ptr_ = ptr_->next; return ret; }
+		
+		reference_type operator*()
+		{ return ptr_->data; }
+
+		reference_type operator*() const
+		{ return ptr_->data; }
+		
+		bool operator!=(const list_iterator& rhs)
+		{ return ptr_ != rhs.ptr_; }
 	private:	
 		node_ptr<T> ptr_;
 };
+
 
 template<typename T>
 class list
 {
 	public:
+		using iterator = list_iterator<T>;
+		using const_iterator = list_iterator<T, true>;
 		list(int capacity = DEFAULT_CAPACITY);
 		void add(const T elements[], int n);
-		bool empty(void) const { return head_ == tail_;}
+		bool empty(void) const { return head_ == nullptr;}
 		bool full(void) const { return count_ == capacity_; }
+		iterator begin(void) { return iterator(head_);}
+		iterator end(void) { return iterator(tail_->next);}
+		const_iterator begin(void) const { return const_iterator(head_); };
+		const_iterator end(void) const;
 	private:
 		node_ptr<T> head_;
 		node_ptr<T> tail_;
@@ -49,6 +81,49 @@ class list
 		static const int DEFAULT_CAPACITY;
 		int capacity_;
 };
+
+template<typename T> const int list<T>::DEFAULT_CAPACITY = 8; 
+
+template<typename T>
+list<T>::list(int capacity)
+	:head_(nullptr),
+	tail_(nullptr),
+	count_(0),
+	capacity_(capacity)
+{
+}
+
+template<typename T>
+void list<T>::add(const T elements[], int n)
+{
+	for(int i = 0; i < n && !full(); ++i)
+	{
+		if(empty())
+		{
+			head_ = node_ptr<T>(new node<T>(elements[i]));
+			tail_ = head_;
+		}
+		else
+		{
+			tail_->next = node_ptr<T>(new node<T>(elements[i]));
+			tail_ = tail_->next;
+		}
+		count_++;
+	}
+}
+
+template<typename T>
+typename list<T>::const_iterator list<T>::end(void) const 
+{
+	if(!empty())
+	{
+		return const_iterator(tail_->next);
+	}
+	else
+	{
+		return const_iterator(head_);
+	}
+}
 
 }
 
